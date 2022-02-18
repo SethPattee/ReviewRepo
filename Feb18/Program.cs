05b90321e69e50886787a75494f4115f0aac26b9
SethPattee <97485847+SethPattee@users.noreply.github.com>
2022-02-18 07:49:45 -0700

    Add files via upload

---
+++ Feb18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main T";
+            Console.WriteLine(mainThread.Name);
+
+            Thread thread1 = new Thread(CountDown);
+            Thread thread2 = new Thread(CountUp);
+            thread1.Start();
+            thread2.Start();
+
+            // CountDown();
+            // CountUp();
+            Console.WriteLine(mainThread.Name + " is complete!");
+        }
+
+        public static void CountDown()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("Timer #1: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Timer #1 is comeplete!");
+        }
+
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Timer #2: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Timer #2 is comeplete!");
+        }
+    }
+}

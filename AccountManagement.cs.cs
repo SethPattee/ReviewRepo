b3da4c0c1d0b7ba0e4fd5c3c30aa963f2bdd270f
SethPattee <97485847+SethPattee@users.noreply.github.com>
2022-02-24 23:32:07 -0700

    Add files via upload

---
+++ AccountManagement.cs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CustomerBanking;
+namespace CustomerBanking
+{
+    public class Account
+    {
+        private decimal balance = 0;
+        public void PayInFunds(decimal amount)
+        {
+            balance = balance + amount;
+        }
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+        public bool WithDrawFunds(decimal amount)
+        {
+            if (amount < 0)
+            {
+                return false;
+            }
+            if (balance >= amount)
+            {
+                balance = balance - amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
+
+class AccountTest
+{
+    public static void Main()
+    {
+        Account test = new Account();
+        test.PayInFunds(50);
+        Console.WriteLine("Balance:" + test.GetBalance());
+    }
+}
